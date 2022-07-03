46138093327ca8a1d87ab0416d584e08f4fd5d27
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-03 03:15:09 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Принимает позицию элемента в двумерном массиве и выводит значение этого элемента.
+
+
+
+int m = 3, n = 4;
+int[,] array = new int[m,n];
+
+Console.WriteLine("Введите индекс элемента в двумерном масиве по Х");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите индекс элемента в двумерном масиве по Y");
+int y = int.Parse(Console.ReadLine());
+
+if (x <= array.GetLength(0) - 1 && y <= array.GetLength(1) - 1)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+       {
+            array[i,j] = new Random().Next(-99,100);
+            Console.Write(array[i,j] + " ");
+       }
+      Console.WriteLine();
+    }
+
+Console.WriteLine("________________");
+Console.WriteLine(array[x,y]);
+}
+
+else
+{
+    Console.WriteLine("Такого элемента в массиве нет");
+}
